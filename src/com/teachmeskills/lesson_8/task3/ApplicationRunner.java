9605fddc41a07efc7e337f8aa368e532777b23d8
Sergey <berdnikausiarhei@gmail.com>
2024-10-28 11:21:31 +0300

    TMC-C32-HM-8-Task3
    A try-catch block has been added

--- src/com/teachmeskills/lesson_8/task3/ApplicationRunner.java
+++ src/com/teachmeskills/lesson_8/task3/ApplicationRunner.java
@@ -5,12 +5,16 @@ import com.teachmeskills.lesson_8.task3.fruit.Apple;
 import java.lang.reflect.Field;
 
 public class ApplicationRunner {
-    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
-        Apple apple = new Apple();
-        Field field = apple.getClass().getDeclaredField("color");
-        field.setAccessible(true);
-        System.out.println("Current color apple: " +  field.get(apple));
-        field.set(apple, "Red");
-        System.out.println("After change color apple: " +  field.get(apple));
+    public static void main(String[] args) {
+       try {
+           Apple apple = new Apple();
+           Field field = apple.getClass().getDeclaredField("color");
+           field.setAccessible(true);
+           System.out.println("Current color apple: " +  field.get(apple));
+           field.set(apple, "Red");
+           System.out.println("After change color apple: " +  field.get(apple));
+       }catch (NoSuchFieldException | IllegalAccessException e) {
+           System.out.println(e.getMessage());
+       }
     }
 }
